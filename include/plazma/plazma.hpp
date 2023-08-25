0dc1b605db637f92cec2c78648b58ae929e4ce77
Kurt Böhm <kurbo96@gmail.com>
2023-08-25 19:11:06 +0200

    Add cumulative header

---
+++ include/plazma/plazma.hpp
@@ -0,0 +1,14 @@
+#ifndef INCLUDE_PLAZMA_PLAZMA_HPP
+#define INCLUDE_PLAZMA_PLAZMA_HPP
+
+#include "base/block.hpp" // IWYU pragma: export
+#include "base/defs.hpp" // IWYU pragma: export
+#include "base/exception.hpp" // IWYU pragma: export
+#include "base/filters.hpp" // IWYU pragma: export
+#include "base/stream.hpp" // IWYU pragma: export
+#include "decode/block.ipp" // IWYU pragma: export
+#include "decode/decode.hpp" // IWYU pragma: export
+#include "decode/read-index.hpp" // IWYU pragma: export
+#include "decode/reader.hpp" // IWYU pragma: export
+
+#endif // INCLUDE_PLAZMA_PLAZMA_HPP
