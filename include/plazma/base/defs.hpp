9e7e16e3287757e61577d7acd6c949a26fa3bd46
Kurt Böhm <kurbo96@gmail.com>
2025-08-03 18:02:44 +0200

    Update enum formatting

--- include/plazma/base/defs.hpp
+++ include/plazma/base/defs.hpp
@@ -22,20 +22,20 @@ struct fmt::formatter<lzma_ret> : formatter<fmt::string_view> {
   auto format(lzma_ret ret, fmt::format_context& ctx) const {
     string_view name = "unknown";
     switch (ret) {
-    case LZMA_OK: name = "LZMA_OK"; break;
-    case LZMA_STREAM_END: name = "LZMA_STREAM_END"; break;
-    case LZMA_NO_CHECK: name = "LZMA_NO_CHECK"; break;
-    case LZMA_UNSUPPORTED_CHECK: name = "LZMA_UNSUPPORTED_CHECK"; break;
-    case LZMA_GET_CHECK: name = "LZMA_GET_CHECK"; break;
-    case LZMA_MEM_ERROR: name = "LZMA_MEM_ERROR"; break;
-    case LZMA_MEMLIMIT_ERROR: name = "LZMA_MEMLIMIT_ERROR"; break;
-    case LZMA_FORMAT_ERROR: name = "LZMA_FORMAT_ERROR"; break;
-    case LZMA_OPTIONS_ERROR: name = "LZMA_OPTIONS_ERROR"; break;
-    case LZMA_DATA_ERROR: name = "LZMA_DATA_ERROR"; break;
-    case LZMA_BUF_ERROR: name = "LZMA_BUF_ERROR"; break;
-    case LZMA_PROG_ERROR: name = "LZMA_PROG_ERROR"; break;
-    case LZMA_SEEK_NEEDED: name = "LZMA_SEEK_NEEDED"; break;
-    default: break;
+      case LZMA_OK: name = "LZMA_OK"; break;
+      case LZMA_STREAM_END: name = "LZMA_STREAM_END"; break;
+      case LZMA_NO_CHECK: name = "LZMA_NO_CHECK"; break;
+      case LZMA_UNSUPPORTED_CHECK: name = "LZMA_UNSUPPORTED_CHECK"; break;
+      case LZMA_GET_CHECK: name = "LZMA_GET_CHECK"; break;
+      case LZMA_MEM_ERROR: name = "LZMA_MEM_ERROR"; break;
+      case LZMA_MEMLIMIT_ERROR: name = "LZMA_MEMLIMIT_ERROR"; break;
+      case LZMA_FORMAT_ERROR: name = "LZMA_FORMAT_ERROR"; break;
+      case LZMA_OPTIONS_ERROR: name = "LZMA_OPTIONS_ERROR"; break;
+      case LZMA_DATA_ERROR: name = "LZMA_DATA_ERROR"; break;
+      case LZMA_BUF_ERROR: name = "LZMA_BUF_ERROR"; break;
+      case LZMA_PROG_ERROR: name = "LZMA_PROG_ERROR"; break;
+      case LZMA_SEEK_NEEDED: name = "LZMA_SEEK_NEEDED"; break;
+      default: break;
     }
     return fmt::formatter<fmt::string_view>::format(name, ctx);
   }
